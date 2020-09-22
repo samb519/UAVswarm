87dab08f4632a8e07239bfc2fffac5c8799716c9
Michael Fornito <fornitomichael@gmail.com>
2020-09-21 22:53:27 -0400

    Additional Changes to AirSim
    Not enitrely sure to be honest, github just says these need to be added. May have happened after I ran the game

---
+++ UAVAirSwarm/Source/UAVAirSwarm/UAVAirSwarmGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UAVAirSwarmGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UAVAIRSWARM_API AUAVAirSwarmGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

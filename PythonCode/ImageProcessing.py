9947944ef5caeeba5987553a201a903d244f5d85
Joseph Moran <44380816+omone1@users.noreply.github.com>
2020-11-30 21:23:23 -0500

    camera loop

--- PythonCode/ImageProcessing.py
+++ PythonCode/ImageProcessing.py
@@ -68,14 +68,7 @@ def initalizeDrone(client, droneNum, droneNames):
         # reshape array to 4 channel image array H X W X 4
         img_rgb = img1d.reshape(response.height, response.width, 3)
 
-        # original image is fliped vertically
-        # back = os.curdir
-        # os.chdir(r"C:\Users\jmora\OneDrive\Pictures\Saved Pictures")
-        # # write to png
-        # airsim.write_png(os.path.normpath( 'img{}.png'.format(i)), img_rgb)
-
         image = img_rgb
-
 
         # convert the image to grayscale, blur it, and detect edges
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -85,8 +78,40 @@ def initalizeDrone(client, droneNum, droneNames):
         # we'll assume that this is our piece of paper in the image
         cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
+
+        counter = 1;
+
+        while len(cnts) == 0 and counter <= 8:
+            imgs = client.simGetImages([
+                airsim.ImageRequest(str(counter), airsim.ImageType.Scene, False, False)], vehicle_name=name)
+
+            response = imgs[0]
+
+            # get numpy array
+            img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8)
+
+            # reshape array to 4 channel image array H X W X 4
+            img_rgb = img1d.reshape(response.height, response.width, 3)
+
+            if i == 0:
+                imgArray = img.image_data_uint8
+            else:
+                imgArray = imgArray + img.image_data_uint8
+
+            image = img_rgb
+
+            # convert the image to grayscale, blur it, and detect edges
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(gray, (5, 5), 0)
+            edged = cv2.Canny(gray, 35, 125)
+
+            # find the contours in the edged image and keep the largest one;
+
+            cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            cnts = imutils.grab_contours(cnts)
+
+            counter += counter
         c = max(cnts, key = cv2.contourArea)
-        print(c)
         # compute the bounding box of the of the paper region and return it
         rresult = cv2.minAreaRect(c)
 
@@ -96,14 +121,11 @@ def initalizeDrone(client, droneNum, droneNames):
         orig = (img_rgb.shape[1], img_rgb.shape[0]);  ## bottom-right of the img
 
         dist = math.sqrt((pt[0] - orig[0]) ** 2 + (pt[1] - orig[1]) ** 2)
-        print(dist)
+
 
         cv2.imshow("edged",edged)
         cv2.imwrite("contoured.png",edged)
         cv2.waitKey(0)
-         # assuming, there is
-
-
 
         inches = (droneWidth * focalLength) / rresult[1][0]
 
@@ -115,6 +137,7 @@ def initalizeDrone(client, droneNum, droneNames):
                     2.0, (0, 255, 0), 3)
         cv2.imshow("image", image)
         cv2.waitKey(0)
+
 
 
 

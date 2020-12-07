12f2021b46c87a5254e964ace5c9d64c85e1236b
WhiskeyEcho125 <44586702+WhiskeyEcho125@users.noreply.github.com>
2020-12-06 20:12:13 -0500

    Update MasterDrone.py

--- AirSim/Source/Senior Design/MasterDrone.py
+++ AirSim/Source/Senior Design/MasterDrone.py
@@ -6,12 +6,15 @@ from imutils import paths
 import imutils
 import MoveToFormationPos
 import ImageProcessing
+import Drone
 
 class MasterDrone:
 	mass = 20
 	viewFromDrone = bytearray()
 	childDrones = []
 	droneGlobalPos = [0, 0, 0]
+	for i in range(10):
+		enrollChildDrone(Drone(i, 5))
 
 	def __init__(self):
 

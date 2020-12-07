7153c956117a4963740df86f23096d1c16ea1161
Joseph Moran <44380816+omone1@users.noreply.github.com>
2020-12-06 20:58:25 -0500

    new code

---
+++ PythonCode/Drone.py
@@ -0,0 +1,39 @@
+import airsim
+import cv2
+import os
+import numpy as np
+from imutils import paths
+import imutils
+import MoveToFormationPos
+import MasterDrone
+import AbsolutePositionAlgorithm
+import ForceControlAlgorithm
+
+class Drone:
+	position = [0, 0, 0]
+
+	def __init__(self, id, name, mass):
+		self.id = id
+		self.name = name
+		self.mass = mass	
+		self.drone = airsim.MultirotorClient();
+    	self.drone.confirmConnection()
+    	self.drone.enableApiControl(True)
+    	self.drone.armDisarm(True)
+
+    	def sendImageToMaster():
+
+	def sendGlobalPositionToMaster():
+		return position
+
+	#drone, drone, vector
+	def setDronePosition(originDrone, targetDrone, position):
+		AbsolutePositionAlgorithm.addDronesToDict(MasterDrone.getChildren())
+
+	#vector
+	def moveDrone(location):
+		self.drone.moveToPosition(location[0], location[1], location[2]).join()
+
+	#float
+	def tick(deltaSeconds):
+
